1dd899f6d5f855adbeebc9526c6e1a6a01eec1f6
ghoul-sol <ghoul.sol@pm.me>
2022-03-11 18:53:57 -0600

    do not renounceRole if newOwner == deployer

--- deploy/arbitrumRinkeby/TreasureMarketplace.deploy.ts
+++ deploy/arbitrumRinkeby/TreasureMarketplace.deploy.ts
@@ -64,7 +64,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     }
 
     // if new owner is set, remove original owner
-    if(await read('TreasureMarketplace', 'hasRole', TREASURE_MARKETPLACE_ADMIN_ROLE, deployer)) {
+    if(await read('TreasureMarketplace', 'hasRole', TREASURE_MARKETPLACE_ADMIN_ROLE, deployer) && newOwner != deployer) {
       await execute(
         'TreasureMarketplace',
         { from: deployer, log: true },

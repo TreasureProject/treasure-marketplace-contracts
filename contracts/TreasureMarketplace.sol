db580a4c44d8725800bbba5faa7583bece241918
Vinny <vinny@treasure.lol>
2022-03-17 13:48:56 -0700

    Implementation with tests

--- contracts/TreasureMarketplace.sol
+++ contracts/TreasureMarketplace.sol
@@ -31,6 +31,13 @@ contract TreasureMarketplace is AccessControlEnumerableUpgradeable, PausableUpgr
         uint64 expirationTime;
     }
 
+    struct CollectionOwnerFee {
+        /// @dev the fee, out of 10,000, that this collection owner will be given for each sale
+        uint32 fee;
+        /// @dev the recipient of the collection specific fee
+        address recipient;
+    }
+
     enum TokenApprovalStatus {NOT_APPROVED, ERC_721_APPROVED, ERC_1155_APPROVED}
 
     /// @notice TREASURE_MARKETPLACE_ADMIN_ROLE role hash
@@ -46,13 +53,16 @@ contract TreasureMarketplace is AccessControlEnumerableUpgradeable, PausableUpgr
     /// @notice the maximum fee which the owner may set (in units of basis points)
     uint256 public constant MAX_FEE = 1500;
 
+    /// @notice the maximum fee which the collection owner may set
+    uint256 public constant MAX_COLLECTION_FEE = 750;
+
     /// @notice the minimum price for which any item can be sold
     uint256 public constant MIN_PRICE = 1e9;
 
     /// @notice which token is used for marketplace sales and fee payments
     IERC20Upgradeable public paymentToken;
 
-    /// @notice fee portion (in basis points) for each sale, (e.g. a value of 100 is 100/10000 = 1%)
+    /// @notice fee portion (in basis points) for each sale, (e.g. a value of 100 is 100/10000 = 1%). This is the fee if no collection owner fee is set.
     uint256 public fee;
 
     /// @notice address that receives fees
@@ -64,9 +74,25 @@ contract TreasureMarketplace is AccessControlEnumerableUpgradeable, PausableUpgr
     /// @notice NFTs which the owner has approved to be sold on the marketplace, maps: nftAddress => status
     mapping(address => TokenApprovalStatus) public tokenApprovals;
 
+    /// @notice fee portion (in basis points) for each sale. This is used if a separate fee has been set for the collection owner.
+    uint256 public feeWithCollectionOwner;
+
+    /// @notice Maps the collection address to the fees which the collection owner collects. Some collections may not have a seperate fee, such as those owned by the Treasure DAO.
+    mapping(address => CollectionOwnerFee) public collectionToCollectionOwnerFee;
+
     /// @notice The fee portion was updated
     /// @param  fee new fee amount (in units of basis points)
     event UpdateFee(uint256 fee);
+
+    /// @notice The fee portion was updated for collections that have a collection owner.
+    /// @param  fee new fee amount (in units of basis points)
+    event UpdateFeeWithCollectionOwner(uint256 fee);
+
+    /// @notice A collection's fees have changed
+    /// @param  _collection  The collection
+    /// @param  _recipient   The recipient of the fees. If the address is 0, the collection fees for this collection have been removed.
+    /// @param  _fee         The fee amount (in units of basis points)
+    event UpdateCollectionOwnerFee(address _collection, address _recipient, uint256 _fee);
 
     /// @notice The fee recipient was updated
     /// @param  feeRecipient the new recipient to get fees
@@ -157,7 +183,7 @@ contract TreasureMarketplace is AccessControlEnumerableUpgradeable, PausableUpgr
         _setRoleAdmin(TREASURE_MARKETPLACE_ADMIN_ROLE, TREASURE_MARKETPLACE_ADMIN_ROLE);
         _grantRole(TREASURE_MARKETPLACE_ADMIN_ROLE, msg.sender);
 
-        setFee(_initialFee);
+        setFee(_initialFee, _initialFee);
         setFeeRecipient(_initialFeeRecipient);
         paymentToken = _initialPaymentToken;
     }
@@ -309,11 +335,7 @@ contract TreasureMarketplace is AccessControlEnumerableUpgradeable, PausableUpgr
             revert("TreasureMarketplace: token is not approved for trading");
         }
 
-        // Handle purchase price payment
-        uint256 totalPrice = listedItem.pricePerItem * _quantity;
-        uint256 feeAmount = totalPrice * fee / BASIS_POINTS;
-        paymentToken.safeTransferFrom(_msgSender(), feeReceipient, feeAmount);
-        paymentToken.safeTransferFrom(_msgSender(), _owner, totalPrice - feeAmount);
+        _payFeesAndSeller(listedItem, _quantity, _nftAddress, _owner);
 
         // Announce sale
         emit ItemSold(
@@ -333,15 +355,70 @@ contract TreasureMarketplace is AccessControlEnumerableUpgradeable, PausableUpgr
         }
     }
 
+    /// @dev pays the fees to the marketplace fee recipient, the collection recipient if one exists, and to the seller of the item.
+    /// @param _listedItem the item that is being purchased
+    /// @param _quantity the quantity of the item being purchased
+    /// @param _collectionAddress the collection to which this item belongs
+    /// @param _seller the seller of the item
+    function _payFeesAndSeller(Listing memory _listedItem, uint256 _quantity, address _collectionAddress, address _seller) private {
+        // Handle purchase price payment
+        uint256 _totalPrice = _listedItem.pricePerItem * _quantity;
+
+        address _collectionFeeRecipient = collectionToCollectionOwnerFee[_collectionAddress].recipient;
+
+        uint256 _protocolFee;
+        uint256 _collectionFee;
+
+        if(_collectionFeeRecipient != address(0)) {
+            _protocolFee = feeWithCollectionOwner;
+            _collectionFee = collectionToCollectionOwnerFee[_collectionAddress].fee;
+        } else {
+            _protocolFee = fee;
+            _collectionFee = 0;
+        }
+
+        uint256 _protocolFeeAmount = _totalPrice * _protocolFee / BASIS_POINTS;
+        uint256 _collectionFeeAmount = _totalPrice * _collectionFee / BASIS_POINTS;
+
+        if(_protocolFeeAmount > 0) {
+            paymentToken.safeTransferFrom(_msgSender(), feeReceipient, _protocolFeeAmount);
+        }
+        if(_collectionFeeAmount > 0) {
+            paymentToken.safeTransferFrom(_msgSender(), _collectionFeeRecipient, _collectionFeeAmount);
+        }
+
+        // Transfer rest to seller
+        paymentToken.safeTransferFrom(_msgSender(), _seller, _totalPrice - _protocolFeeAmount - _collectionFeeAmount);
+    }
+
     // Owner administration ////////////////////////////////////////////////////////////////////////////////////////////
 
-    /// @notice Updates the fee amount which is collected during sales
-    /// @dev    This is callable only by the owner. Fee may not exceed MAX_FEE
+    /// @notice Updates the fee amount which is collected during sales, for both collections with and without owner specific fees.
+    /// @dev    This is callable only by the owner. Both fees may not exceed MAX_FEE
     /// @param  _newFee the updated fee amount is basis points
-    function setFee(uint256 _newFee) public onlyRole(TREASURE_MARKETPLACE_ADMIN_ROLE) {
-        require(_newFee <= MAX_FEE, "TreasureMarketplace: max fee");
+    function setFee(uint256 _newFee, uint256 _newFeeWithCollectionOwner) public onlyRole(TREASURE_MARKETPLACE_ADMIN_ROLE) {
+        require(_newFee <= MAX_FEE && _newFeeWithCollectionOwner <= MAX_FEE, "TreasureMarketplace: max fee");
+
         fee = _newFee;
+        feeWithCollectionOwner = _newFeeWithCollectionOwner;
+
         emit UpdateFee(_newFee);
+        emit UpdateFeeWithCollectionOwner(_newFeeWithCollectionOwner);
+    }
+
+    /// @notice Updates the fee amount which is collected during sales fro a specific collection
+    /// @dev    This is callable only by the owner
+    /// @param  _collectionAddress The collection in question. This must be whitelisted.
+    /// @param _collectionOwnerFee The fee and recipient for the collection. If the 0 address is passed as the recipient, collection specific fees will not be collected.
+    function setCollectionOwnerFee(address _collectionAddress, CollectionOwnerFee calldata _collectionOwnerFee) external onlyRole(TREASURE_MARKETPLACE_ADMIN_ROLE) {
+        require(tokenApprovals[_collectionAddress] == TokenApprovalStatus.ERC_1155_APPROVED
+            || tokenApprovals[_collectionAddress] == TokenApprovalStatus.ERC_721_APPROVED, "TreasureMarketplace: Collection is not approved");
+        require(_collectionOwnerFee.fee <= MAX_COLLECTION_FEE, "TreasureMarketplace: Collection fee too high");
+
+        // The collection recipient can be the 0 address, meaning we will treat this as a collection with no collection owner fee.
+        collectionToCollectionOwnerFee[_collectionAddress] = _collectionOwnerFee;
+
+        emit UpdateCollectionOwnerFee(_collectionAddress, _collectionOwnerFee.recipient, _collectionOwnerFee.fee);
     }
 
     /// @notice Updates the fee recipient which receives fees during sales
